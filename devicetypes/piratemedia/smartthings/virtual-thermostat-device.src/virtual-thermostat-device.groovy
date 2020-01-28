bfcc6c60a61dd016e145ae90312f6f7c618bbcec
mstoute57 <60303113+mstoute57@users.noreply.github.com>
2020-01-28 08:32:30 -0500

    added heating setpoint

--- devicetypes/piratemedia/smartthings/virtual-thermostat-device.src/virtual-thermostat-device.groovy
+++ devicetypes/piratemedia/smartthings/virtual-thermostat-device.src/virtual-thermostat-device.groovy
@@ -28,6 +28,8 @@ metadata {
 		command "setHeatingStatus", ["boolean"]
 		command "setEmergencyMode", ["boolean"]
 		command "setHeatingOff", ["boolean"]
+		command "setHeatingSetpoint", ["number"]
+		command "setCoolingSetpoint", ["number"]
         
 		attribute "temperatureUnit", "string"
 		attribute "targetTemp", "string"
